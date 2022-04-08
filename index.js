f3b28fe11e2c08b058a701601bb74e0ae9b92ba7
timaBima <timothy.smirnov@gmail.com>
2022-04-07 21:22:20 -0700

    centered button, js adds poem inside of parent div

--- index.js
+++ index.js
@@ -13,44 +13,46 @@ function darkMode() {
 
 const api_url = "https://www.poemist.com/api/v1/randompoems";
 
-//create async function to fetch poem and save json data as variable
-const getPoem = async () => {
-	  const response = await fetch(api_url);
-	  const data = await response.json();
-	  return JSON.stringify(data);
-}; 
-
-
-//display the poem as a div in the body with title and author only when button clicked
-function displayPoem() {
-	getPoem().then(data => {
-		let poem = JSON.parse(data);
-	
-		console.log(poem[0]);
-		let poem_title = poem[0].title;
-		let poem_author = poem[0].poet.name;
-		let poem_url= poem[0].poet.url;
-		let poem_text = poem[0].content;
-		//replace all newlines in content with <br>
-		let poem_text_br = poem_text.replace(/\n/g, "<br>");
-		//creates a div with title, poem and author  	
-		let poem_div = document.createElement("div");
-		poem_div.classList.add("poem");
-		poem_div.innerHTML = `<h2>${poem_title}</h2><p style={{whiteSpace: 'pre-line'}}>${poem_text_br}</p><em><p>${poem_author}</p></em><a href=${poem_url}>Read more on Poemist</a>`;
-		document.body.append(poem_div);
-	//if poem is not found, display error message
-	}).catch(error => {
-		let poem_div = document.createElement("div");
-		poem_div.classList.add("poem");
-		poem_div.innerHTML = `<h2>Error</h2><p>No poem found</p>`;
-		document.body.append(poem_div);
-	});
-}
-
 //defines poem button and adds event listener
 const poemBtn = document.getElementById("poemBtn");
 poemBtn.addEventListener("click", displayPoem);
 
+//create async function to fetch poem and save json data as variable
+const getPoem = async () => {
+  const response = await fetch(api_url);
+  const data = await response.json();
+  return JSON.stringify(data);
+};
+
+//display the poem as a div in the body with title and author only when button clicked
+function displayPoem() {
+  getPoem()
+    .then((data) => {
+      let poem = JSON.parse(data);
+
+      console.log(poem[0]);
+      let poem_title = poem[0].title;
+      let poem_author = poem[0].poet.name;
+      let poem_url = poem[0].poet.url;
+      let poem_text = poem[0].content;
+
+      let poem_text_br = poem_text.replace(/\n/g, "<br>");
+      //creates a div with title, poem and author
+      let poem_div = document.createElement("div");
+      poem_div.classList.add("poem");
+      poem_div.innerHTML = `<h2>${poem_title}</h2><p style={{whiteSpace: 'pre-line'}}>${poem_text_br}</p><em><p>${poem_author}</p></em><a href=${poem_url}>Read more on Poemist</a>`;
+      //append child to div with class "poemDisplay"
+      document.getElementsByClassName("poemDisplay")[0].appendChild(poem_div);
+      return poem;
+      //if poem is not found, display error message
+    })
+    .catch((error) => {
+      let poem_div = document.createElement("div");
+      poem_div.classList.add("poem");
+      poem_div.innerHTML = `<h2>Error</h2><p>No poem found</p>`;
+      document.getElementsByClassName("poemDisplay")[0].appendChild(poem_div);
+    });
+}
 
 /* getPoem().then(poem => {
 	  const poemDiv = document.createElement("div");
