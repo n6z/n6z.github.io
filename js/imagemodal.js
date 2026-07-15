3af53db790570806be2b66ad57089e40f25a4eeb
timaBima <timothy.smirnov@gmail.com>
2026-07-15 14:18:01 -0700

    photo navigation js

--- js/imagemodal.js
+++ js/imagemodal.js
@@ -1,15 +1,21 @@
 const images = document.querySelectorAll(".gallery__item img");
+const imagesArray = Array.from(images);
 let imgSrc;
+let currentIndex;
+let keydownHandler;
+
 // get images src onclick
-images.forEach((img) => {
+images.forEach((img, index) => {
   img.addEventListener("click", (e) => {
     imgSrc = e.target.src;
+    currentIndex = index;
     //run modal function
-    imgModal(imgSrc);
+    imgModal(imgSrc, currentIndex);
   });
 });
+
 //creating the modal
-let imgModal = (src) => {
+let imgModal = (src, index) => {
   const modal = document.createElement("div");
   modal.setAttribute("class", "modal");
   //add the modal to the main section or the parent element
@@ -17,15 +23,63 @@ let imgModal = (src) => {
   //adding image to modal
   const newImage = document.createElement("img");
   newImage.setAttribute("src", src);
+  newImage.setAttribute("class", "modal-img");
+  
   //creating the close button
   const closeBtn = document.createElement("i");
   closeBtn.setAttribute("class", "fas fa-times closeBtn");
+  
+  //creating navigation buttons
+  const prevBtn = document.createElement("i");
+  prevBtn.setAttribute("class", "fas fa-chevron-left prevBtn");
+  
+  const nextBtn = document.createElement("i");
+  nextBtn.setAttribute("class", "fas fa-chevron-right nextBtn");
+  
+  //navigation function
+  const navigate = (direction) => {
+    if (direction === 'next') {
+      currentIndex = (currentIndex + 1) % imagesArray.length;
+    } else {
+      currentIndex = (currentIndex - 1 + imagesArray.length) % imagesArray.length;
+    }
+    newImage.src = imagesArray[currentIndex].src;
+  };
+  
+  //navigation button click handlers
+  prevBtn.onclick = (e) => {
+    e.stopPropagation();
+    navigate('prev');
+  };
+  
+  nextBtn.onclick = (e) => {
+    e.stopPropagation();
+    navigate('next');
+  };
+  
+  //keyboard navigation
+  keydownHandler = (e) => {
+    if (e.key === 'ArrowLeft') {
+      navigate('prev');
+    } else if (e.key === 'ArrowRight') {
+      navigate('next');
+    } else if (e.key === 'Escape') {
+      modal.remove();
+      document.removeEventListener('keydown', keydownHandler);
+    }
+  };
+  document.addEventListener('keydown', keydownHandler);
+  
   //close function
   closeBtn.onclick = () => {
     modal.remove();
+    document.removeEventListener('keydown', keydownHandler);
   };
+  
   modal.onclick = () => {
     modal.remove();
+    document.removeEventListener('keydown', keydownHandler);
   };
-  modal.append(newImage, closeBtn);
+  
+  modal.append(newImage, closeBtn, prevBtn, nextBtn);
 };
